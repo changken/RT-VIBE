177fce293cad20a9d8910f153182d772470ac362
zc402 <zc35402@163.com>
2021-12-02 17:23:05 +0800

    Adjust structure in rt_vibe

--- vibe/rt/rt_vibe.py
+++ vibe/rt/rt_vibe.py
@@ -30,7 +30,38 @@ from vibe.utils.demo_utils import (
 os.environ['PYOPENGL_PLATFORM'] = 'egl'
 
 
+class HiddenStates:
+    """
+    Manage GRU hidden states across multiple target.
+    """
+    def __init__(self):
+        self._hidden = {}  # Dict: {person_id, gru_hidden_state}
+
+    def update_person_id(self, tracking_results):
+        """
+        preserve person_ids which are still being tracked.
+        Init newly tracked people with 'None' state.
+        """
+        new_hidden: dict = {}
+        for person_id in tracking_results.keys():
+            # Not a New person
+            if person_id in self._hidden.keys():  # a person still being tracked
+                new_hidden[person_id] = self._hidden[person_id]
+            else:  # A new person
+                new_hidden[person_id] = None  # use None and pytorch will take care of the initialization.
+        self._hidden = new_hidden  # The lost trackers are deleted
+
+    def get_hidden(self, person_id):
+        return self._hidden[person_id]
+
+    def set_hidden(self, person_id, hidden_state):
+        self._hidden[person_id] = hidden_state
+
+
 class RtVibe:
+    # Color for different person. Do not use this directly, use _mesh_color() instead
+    _mesh_color_20 = {k: colorsys.hsv_to_rgb(np.random.rand(), 0.5, 1.0) for k in range(20)}
+
     @torch.no_grad()
     def __init__(self, render=True):
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -54,7 +85,8 @@ class RtVibe:
         ).to(self.device)
 
         # GRU hidden states for previous tracking person id. dict:{id, hidden}
-        self.prev_tracking_hidden: dict = {}
+        # self.prev_tracking_hidden: dict = {}
+        self.hidden_states = HiddenStates()
 
         pretrained_file = download_ckpt(use_3dpw=False)
         ckpt = torch.load(pretrained_file)
@@ -65,32 +97,26 @@ class RtVibe:
         print(f'Loaded pretrained weights from \"{pretrained_file}\"')
 
         self.renderer = None  # lazy initialize with first image to get W,H
-        # TODO: Fix for unlimited people number
-        self.mesh_color = {k: colorsys.hsv_to_rgb(np.random.rand(), 0.5, 1.0) for k in range(1000)}
 
-    @torch.no_grad()
-    def __call__(self, image: np.ndarray):
-        orig_height, orig_width = image.shape[:2]
-        if self.renderer is None:
-            self.renderer = Renderer(resolution=(orig_width, orig_height), orig_img=True, wireframe=self.wireframe)
+    @classmethod
+    def _mesh_color(cls, k):
+        k = k % 20
+        return cls._mesh_color_20[k]
 
-        # Tracking
-        tracking_results = self.mpt_live(image)
+    def _track(self, image: np.ndarray):
+        # Image to tracking result.
+        tracking_results = self.mpt_live(image)  # BUG: This will always set frame_num to 1.
         # Keep hidden state for each individual.
-        tracking_hidden: dict = {}  # New hidden dict
-        for person_id in tracking_results.keys():
-            # Not a New person
-            if person_id in self.prev_tracking_hidden.keys():
-                tracking_hidden[person_id] = self.prev_tracking_hidden[person_id]
-            else:  # New person
-                tracking_hidden[person_id] = None  # use None and pytorch will take care of the initialization.
-        self.prev_tracking_hidden = tracking_hidden  # The lost trackers are deleted
-        del tracking_hidden
+        self.hidden_states.update_person_id(tracking_results)
+        return tracking_results
+
+    def _vibe_infer(self, image: np.ndarray, track_res: dict):
         # RT-VIBE
+        orig_height, orig_width = image.shape[:2]
         vibe_results = {}
-        for person_id in list(tracking_results.keys()):
-            bboxes = tracking_results[person_id]['bbox']
-            frames = tracking_results[person_id]['frames']
+        for person_id in list(track_res.keys()):
+            bboxes = track_res[person_id]['bbox']
+            frames = track_res[person_id]['frames']
             joints2d = None
             bbox_scale = 1.1
 
@@ -113,7 +139,7 @@ class RtVibe:
             batch_size = 1
 
             # Use corresponding hidden state!
-            self.vibe_model.encoder.gru_final_hidden = self.prev_tracking_hidden[person_id]
+            self.vibe_model.encoder.gru_final_hidden = self.hidden_states.get_hidden(person_id)
             batch = next(iter(dataloader))
             batch = batch.unsqueeze(0)
             batch = batch.to(self.device)
@@ -121,7 +147,7 @@ class RtVibe:
             # batch_size, seqlen = batch.shape[:2]
             output = self.vibe_model(batch)[-1]
             # Update hidden state for person_id
-            self.prev_tracking_hidden[person_id] = self.vibe_model.encoder.gru_final_hidden
+            self.hidden_states.set_hidden(person_id, self.vibe_model.encoder.gru_final_hidden)
 
             # Just to be consist with original format
             pred_cam.append(output['theta'][:, :, :3].reshape(batch_size * seqlen, -1))
@@ -144,7 +170,6 @@ class RtVibe:
             pred_betas = pred_betas.cpu().numpy()
             pred_joints3d = pred_joints3d.cpu().numpy()
             smpl_joints2d = smpl_joints2d.cpu().numpy()
-
 
             orig_cam = convert_crop_cam_to_orig_img(
                 cam=pred_cam,
@@ -173,48 +198,66 @@ class RtVibe:
             }
 
             vibe_results[person_id] = output_dict
+        return vibe_results
+
+    @torch.no_grad()
+    def __call__(self, image: np.ndarray):
+
+        # Tracking
+        tracking_results = self._track(image)
+        # Run VIBE inference
+        vibe_results = self._vibe_infer(image, tracking_results)
 
         # Render
         if self.render:
-            num_frames = 1
-            # ========= Render results as a single video ========= #
+            self._render_and_show(image, vibe_results)
 
-            # prepare results for rendering
-            frame_results = prepare_rendering_results(vibe_results, num_frames)
+        return vibe_results
 
-            img = image
+    def _render_and_show(self, image: np.ndarray, vibe_results: dict):
+        """Render and show render result"""
+        orig_height, orig_width = image.shape[:2]
 
-            for person_id, person_data in frame_results[0].items():
-                frame_verts = person_data['verts']
-                frame_cam = person_data['cam']
+        if self.renderer is None:
+            self.renderer = Renderer(resolution=(orig_width, orig_height), orig_img=True, wireframe=self.wireframe)
 
-                mc = self.mesh_color[person_id]
+        num_frames = 1
+        # ========= Render results as a single video ========= #
 
-                mesh_filename = None
+        # prepare results for rendering
+        frame_results = prepare_rendering_results(vibe_results, num_frames)
 
-                img = self.renderer.render(
-                    img,
+        img = image
+
+        for person_id, person_data in frame_results[0].items():
+            frame_verts = person_data['verts']
+            frame_cam = person_data['cam']
+
+            mc = self._mesh_color(person_id)
+
+            mesh_filename = None
+
+            img = self.renderer.render(
+                img,
+                frame_verts,
+                cam=frame_cam,
+                color=mc,
+                mesh_filename=mesh_filename,
+            )
+
+            if self.sideview:
+                side_img = np.zeros_like(img)
+                side_img = self.renderer.render(
+                    side_img,
                     frame_verts,
                     cam=frame_cam,
                     color=mc,
-                    mesh_filename=mesh_filename,
+                    angle=270,
+                    axis=[0, 1, 0],
                 )
 
-                if self.sideview:
-                    side_img = np.zeros_like(img)
-                    side_img = self.renderer.render(
-                        side_img,
-                        frame_verts,
-                        cam=frame_cam,
-                        color=mc,
-                        angle=270,
-                        axis=[0, 1, 0],
-                    )
+        if self.sideview:
+            img = np.concatenate([img, side_img], axis=1)
 
-            if self.sideview:
-                img = np.concatenate([img, side_img], axis=1)
-
-            cv2.imshow('Video', img)
-            cv2.waitKey(1)
-
-        return vibe_results
+        cv2.imshow('Video', img)
+        cv2.waitKey(1)
